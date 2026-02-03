e09ad12dae5364f6636516ab4cb4c3b93a28e9a1
Marco Alonso <marco.alonso@estudiante.epsum.school>
2026-02-03 18:45:58 +0100

    Initial commit

---
+++ app/src/main/java/com/example/appteatrov1/ConnectionClass.java
@@ -0,0 +1,28 @@
+package com.example.appteatrov1;
+import android.util.Log;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Objects;
+public class ConnectionClass {
+    protected static String db = "estructura";
+
+    protected static String ip = "10.0.2.2";
+
+    protected static String puerto = "3306";
+
+    protected static String usuario = "root";
+
+    protected static String password = "Marco27";
+
+    public Connection CONN() {
+        Connection conn = null;
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            String connectionString = "jdbc:mysql://" + ip + ":" + puerto + "/" + db;
+            conn = DriverManager.getConnection(connectionString,usuario,password);
+        } catch (Exception e) {
+            Log.e("Error", Objects.requireNonNull(e.getMessage()));
+        }
+        return conn;
+    }
+}
---
+++ app/src/main/java/com/example/appteatrov1/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.appteatrov1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class MainActivity extends AppCompatActivity {
+
+    ConnectionClass connectionClass;
+
+    Connection con;
+
+    ResultSet rs;
+
+    String name, str;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        connectionClass = new ConnectionClass();
+        connect();
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void connect() {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.execute(() -> {
+            try {
+                con = connectionClass.CONN();
+                if (con == null) {
+                    str = "Error en conexion con la base de datos";
+                } else {
+                    str = "CONECTADO CON LA BASE DE DATOS";
+                }
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+
+            runOnUiThread(() -> {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Toast.makeText(this, str, Toast.LENGTH_SHORT).show();
+            });
+        });
+    }
+}
