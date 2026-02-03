f8aba4e505ed74fbd4a58f9371ba4665c39a8484
Marco Alonso <marco.alonso@estudiante.epsum.school>
2026-02-03 19:09:41 +0100

    He puesto un botón para que cuando se pulse salga el mensaje de conectado :)

--- app/src/main/java/com/example/appteatrov1/MainActivity.java
+++ app/src/main/java/com/example/appteatrov1/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         connectionClass = new ConnectionClass();
-        connect();
+        findViewById(R.id.button).setOnClickListener(v -> connect());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
